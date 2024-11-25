d0b4863bc48c754adf43bfe8aee601eb28c36746
Hansen <300071283@fvtc.edu>
2024-11-25 16:16:33 -0600

    added app.js, endpoint.rest (added all endpoints), fixed some stuff in foodTruck.js

--- db-connection.js
+++ db-connection.js
@@ -3,6 +3,7 @@ const { uri } = require('./secrets/dbcon.json')
 const client = new MongoClient(uri)
 
 const getCollection = dbName => collectionName => async () =>{
+    console.log(uri)
     await client.connect()
     return client.db(dbName).collection(collectionName)
 }
--- public/scripts/stie.js
+++ public/scripts/stie.js
@@ -10,11 +10,13 @@ const getFoodData = async () => {
     const url = `/api/v1/ ${urlParts[0] === 'event' ? `event/${urlParts[1]}`: urlParts[0] === 'admin' ? '/admin' : ''}` // returns food of the requested id  or returns the admin page
 
     const result = await fetch(url)
-    const {something} = await result.json()
+    const {id, name, description, price, imageUrl} = await result.json()
 
     // TODO: need to decided were this shiz will go aka there is no html atm lmao
 
+    const test = document.querySelector('.temp')
 
+    test.textContent = {id, name, description, price, imageUrl}
 }
 
 getFoodData()
